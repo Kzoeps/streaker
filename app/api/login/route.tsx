6163e30032932c2e841f0ced3f1f8e7d95ebfc8a
kzoeps <kzoepa@gmail.com>
2024-09-15 02:59:21 +0600

    fix: remove routes for now

--- app/api/login/route.tsx
+++
@@ -1,7 +0,0 @@
-import { NextRequest } from "next/server";
-
-export default async function POST(req: NextRequest) {
-    const { email, password } = await req.json();
-    console.log(email, password)
-    return new Response("Login successful");
-}
