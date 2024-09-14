aa50684b653c870c940dd1fdac1e7096ed92f085
kzoeps <kzoepa@gmail.com>
2024-09-15 03:43:48 +0600

    feat: add sign up

--- app/sign-up/[[...slug]]/page.tsx
+++ app/sign-up/[[...slug]]/page.tsx
@@ -1,9 +1,8 @@
-import SignupForm from "@/components/ui/sign-up-form";
-
+import { SignUp as SignUpForm } from "@clerk/nextjs";
 export default function SignUp() {
     return (
         <div className="flex min-h-screen items-center justify-center bg-gray-100">
-            <SignupForm />
+            <SignUpForm />
         </div>
     );
 }
