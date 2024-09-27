7e77111942ac8eb868b9fb063cb91e928f02038f
kzoeps <kzoepa@gmail.com>
2024-09-26 22:40:26 -0400

    feat: resetter logic

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,11 +1,20 @@
 import { Streak } from "@/actions";
 import { sql } from "@vercel/postgres";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+dayjs.extend(utc);
 export async function POST() {
-    const id = "e13d5b68-5775-430c-af50-eefcfdb542b0";
+    const id = "01ee322c-7086-492f-b066-5abbbc99175c";
     const data =
         await sql<Streak>`SELECT streakcount FROM Streaks WHERE id = ${id}`;
-    const streakcount = data.rows?.[0]?.streakcount ?? 0;
-    console.log(streakcount);
-    await sql`UPDATE Streaks SET streakcount = ${streakcount + 1} WHERE id = ${id}`;
+    const streakcount = data.rows[0]?.streakcount ?? 0;
+    const lastCompletedAt = dayjs(data.rows[0]?.last_completed_at);
+    const today = dayjs().utc();
+    if (!lastCompletedAt.isValid()) {
+        return new Response("OK", { status: 200 });
+    }
+    if (today.diff(lastCompletedAt, "minute") > 10 && streakcount !== 0) {
+        await sql<Streak>`UPDATE Streaks SET streakcount = ${0} WHERE id = ${id}`;
+    }
     return new Response("OK", { status: 200 });
 }
