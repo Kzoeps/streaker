46b3f505be2560f36cd9bb8bc2918cf76c945051
kzoeps <kzoepa@gmail.com>
2024-09-26 21:01:38 -0400

    update to post isntead of get

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,10 +1,11 @@
 import { Streak } from "@/actions";
 import { sql } from "@vercel/postgres";
-export async function GET() {
+export async function POST() {
     const id = "e13d5b68-5775-430c-af50-eefcfdb542b0";
     const data =
         await sql<Streak>`SELECT streakcount FROM Streaks WHERE id = ${id}`;
     const streakcount = data.rows?.[0]?.streakcount ?? 0;
+    console.log(streakcount);
     await sql`UPDATE Streaks SET streakcount = ${streakcount + 1} WHERE id = ${id}`;
     return new Response("OK", { status: 200 });
 }
