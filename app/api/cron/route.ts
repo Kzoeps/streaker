c9f141bb4d57f63ac6b33ce3a17480c5cd13b65b
kzoeps <kzoepa@gmail.com>
2024-09-26 23:25:11 -0400

    feat: pdate logs

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -8,14 +8,15 @@ export async function POST() {
     const data =
         await sql<Streak>`SELECT streakcount FROM Streaks WHERE id = ${id}`;
     const streakcount = data.rows[0]?.streakcount ?? 0;
+    console.log(data.rows[0].last_completed_at, "raw last complteeded at");
     const lastCompletedAt = dayjs(data.rows[0]?.last_completed_at);
     const today = dayjs().utc();
     console.log(today.format(), "today");
-    console.log(lastCompletedAt.format());
+    console.log(lastCompletedAt.format(), "lastCompletedAt");
     if (!lastCompletedAt.isValid()) {
         return new Response("OK", { status: 200 });
     }
-    if (today.diff(lastCompletedAt, "minute") > 10 && streakcount !== 0) {
+    if (today.diff(lastCompletedAt, "minute") > 1 && streakcount !== 0) {
         await sql<Streak>`UPDATE Streaks SET streakcount = ${0} WHERE id = ${id}`;
     }
     return new Response("OK", { status: 200 });
