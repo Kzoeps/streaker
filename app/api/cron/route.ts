68c0ebf57e6ebc0a61f4f236beb5ecb7716804ac
kzoeps <kzoepa@gmail.com>
2024-09-26 20:49:53 -0400

    increment by one

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,6 +1,10 @@
+import { Streak } from "@/actions";
 import { sql } from "@vercel/postgres";
 export async function GET() {
     const id = "e13d5b68-5775-430c-af50-eefcfdb542b0";
-    await sql`UPDATE Streaks SET streakcount = 22 WHERE id = ${id}`;
+    const data =
+        await sql<Streak>`SELECT streakcount FROM Streaks WHERE id = ${id}`;
+    const streakcount = data.rows?.[0]?.streakcount ?? 0;
+    await sql`UPDATE Streaks SET streakcount = ${streakcount + 1} WHERE id = ${id}`;
     return new Response("OK", { status: 200 });
 }
