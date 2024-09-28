2202cd946d2e4d35cced336211c01921db23e171
kzoeps <kzoepa@gmail.com>
2024-09-27 22:11:30 -0400

    update icon and style for slider

--- components/ui/streak-card-check-button.tsx
+++ components/ui/streak-card-check-button.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { Check, ChevronRight } from "lucide-react";
+import { ArrowLeftFromLine, Check, ChevronRight } from "lucide-react";
 import { StreakCardProps } from "./streak-card";
 
 export interface StreakCardCheckButtonProps extends StreakCardProps {}
@@ -16,13 +16,13 @@ export const StreakCardCheckButton = ({
                 className={cn(
                     "rounded-full p-1",
                     checked && "bg-green-500",
-                    !checked &&
-                        "border border-gray-300 bg-gray-50 shadow-xl drop-shadow-lg"
+                    !checked && "border border-gray-300 bg-gray-50"
                 )}
             >
                 {checked && <Check className={"h-4 w-4 text-white"} />}
                 {!checked && (
-                    <ChevronRight className="h-4 w-4 text-orange-500" />
+                    // <ChevronRight className="h-4 w-4 text-orange-500" />
+                    <ArrowLeftFromLine className="h-4 w-4 text-orange-500" />
                 )}
             </button>
         </>
